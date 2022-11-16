33bbf496fd15d17a8897c583f67d5e267cf3159c
Odhran <mahonyodhran@gmail.com>
2022-11-16 23:30:39 +0000

    added update route

--- app/api.py
+++ app/api.py
@@ -43,3 +43,16 @@ async def add_todo(todo: dict) -> dict:
     return {
         "data": { "Todo added." }
     }
+    
+@app.put("/todo/{id}", tags=["todos"])
+async def update_todo(id: int, body: dict) -> dict:
+    for todo in todos:
+        if int(todo["id"]) == id:
+            todo["item"] = body["item"]
+            return {
+                "data": f"Todo with id {id} has been updated."
+            }
+
+    return {
+        "data": f"Todo with id {id} not found."
+    }
